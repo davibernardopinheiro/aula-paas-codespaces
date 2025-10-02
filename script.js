a44f984d9d6c913e39e37885831e5e13f3b4b4df
davibernardopinheiro <davibernardo.pinheiro@gmail.com>
2025-10-02 15:18:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+JavaScript
+document.addEventListener('DOMContentLoaded', () => {
+const conselhos = [
+"Na dúvida, reinicie o servidor.",
+"Commit mais vezes. Seus futuros 'eu' e 'colegas' agradecerão.",
+"Leia a documentação. Sério, leia.",
+"Antes de otimizar, faça funcionar."
+];
+const textoConselho = document.getElementById('conselho-texto');
+const botaoNovoConselho = document.getElementById('novo-conselho-btn');
+function gerarConselho() {
+const indiceAleatorio = Math.floor(Math.random() * conselhos.length);
+textoConselho.textContent = conselhos[indiceAleatorio];
+}
+botaoNovoConselho.addEventListener('click', gerarConselho);
+});
